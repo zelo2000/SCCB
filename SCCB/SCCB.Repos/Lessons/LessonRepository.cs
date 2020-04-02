1a79414ba90e6ca68fd4536994bb0cf050bc6532
Sofiia Romah <sofija.romah@gmail.com>
2020-04-02 14:56:41 +0300

    Created method Find lessons by group id and test to it

--- SCCB/SCCB.Repos/Lessons/LessonRepository.cs
+++ SCCB/SCCB.Repos/Lessons/LessonRepository.cs
@@ -3,6 +3,8 @@ using SCCB.DAL;
 using SCCB.DAL.Entities;
 using SCCB.Repos.Generic;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SCCB.Repos.Lessons
@@ -14,6 +16,11 @@ namespace SCCB.Repos.Lessons
         public LessonRepository(SCCBDbContext dbContext) : base(dbContext)
         {
             _dbContext = dbContext;
+        }
+
+        public async Task<List<Lesson>> FindLessonsByGroupIdAsync(Guid id)
+        {
+            return await _dbContext.Lessons.Where(x => x.GroupId == id).ToListAsync();
         }
     }
 }
